85af277337464218751ce62adb2c855c109020fe
KingCosmic <kingcosmic2016@gmail.com>
2017-08-09 14:28:38 -0500

    added more properties to the msg object and allowed multiple callbacks

--- client.js
+++ client.js
@@ -4,41 +4,99 @@ const middleware = line.middleware
 
 function Client(config) {
 
+  var that = this;
+
   this.app = express();
 
   this.client = new line.Client({
     channelAccessToken: config.channelAccessToken
   });
 
+  this.message = []
+
   this.on = function(event, callback) {
-    if (event == "message") this.message = callback;
+    if (event == "message") this.message.push(callback);
+  }
+
+  this.event = function(event) {
+
+    var args = Array.from(arguments);
+
+    args.splice(0, 1);
+
+    if (event == "message") {
+      if (this.message.length != 0) {
+        this.message.forEach(function(callback) {
+          callback.apply(null, args)
+        });
+      }
+    }
   }
 
   this.app.use(middleware(config))
 
   this.app.post('/webhook', (req, res) => {
-    //res.json(req.body.events) // req.body will be webhook event object
-
-    var that = this;
 
     var events = req.body.events;
 
     if (events.length == 1) {
       var event = events[0];
 
+      console.log(event)
+
       if (event.type == "message" && event.message.type == "text") {
         // check for commands and change the response msg
 
-        var msg = {
-          content: event.message.text,
-          reply: function(content) {
-            that.client.replyMessage(event.replyToken, {type: "text", text: content})
-          }
-        }
+        this.client.getProfile(event.source.userId)
+        .then((profile) => {
 
-        if (this.message) {
-          this.message(msg);
-        }
+          var msg;
+
+          if (event.source.type == "group") {
+            msg = {
+              content: event.message.text,  // the message content
+              author: {
+                displayName: profile.displayName,
+                id: profile.userId,
+                pictureUrl: profile.pictureUrl,
+                statusMessage: profile.statusMessage,
+                sendMessage: function(content) {
+                  that.client.pushMessage(profile.userId, {type: "text", text: content});
+                }
+              },
+              group: {
+                id: event.source.groupId,
+
+                leave: function () {
+                  that.client.leaveGroup(`${this.id}`);
+                }
+
+              },
+              reply: function(content) {
+                that.client.replyMessage(event.replyToken, {type: "text", text: content});
+              }
+            }
+          } else {
+            msg = {
+              content: event.message.text,  // the message content
+              author: {
+                displayName: profile.displayName,
+                id: profile.userId,
+                pictureUrl: profile.pictureUrl,
+                statusMessage: profile.statusMessage,
+                sendMessage: function(content) {
+                  that.client.pushMessage(profile.userId, {type: "text", text: content});
+                }
+              },
+              reply: function(content) {
+                that.client.replyMessage(event.replyToken, {type: "text", text: content});
+              }
+            }
+          }
+
+          this.event("message", msg);
+
+        })
 
       }
 
